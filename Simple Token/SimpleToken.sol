809ea95f293a8f8b7b176ce135ddd5a376fd93c3
mspaintenjoyer <2022ebcs158@online.bits-pilani.ac.in>
2025-08-25 15:16:46 +0530

    Update SimpleToken.sol

--- Simple Token/SimpleToken.sol
+++ Simple Token/SimpleToken.sol
@@ -1,11 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-/**
- * @title SimpleToken
- * @dev A very basic, non-standard token contract.
- * It demonstrates how to manage a supply and balances using a mapping.
- */
+
 contract SimpleToken {
     // State variables
     string public name = "MySimpleToken";
@@ -20,36 +16,24 @@ contract SimpleToken {
     // A simple event to log token transfers.
     event Transfer(address indexed from, address indexed to, uint256 amount);
 
-    /**
-     * @dev The constructor mints the initial supply of tokens and assigns them to the contract creator.
-     * @param initialSupply The total number of tokens to create.
-     */
+
     constructor(uint256 initialSupply) {
         _totalSupply = initialSupply;
         _balances[msg.sender] = initialSupply;
         emit Transfer(address(0), msg.sender, initialSupply);
     }
 
-    /**
-     * @dev Returns the total supply of tokens.
-     */
+
     function totalSupply() public view returns (uint256) {
         return _totalSupply;
     }
 
-    /**
-     * @dev Returns the token balance of a specific address.
-     * @param account The address to query.
-     */
+
     function balanceOf(address account) public view returns (uint256) {
         return _balances[account];
     }
 
-    /**
-     * @dev Transfers a specified amount of tokens to a recipient.
-     * @param to The address of the recipient.
-     * @param amount The amount of tokens to transfer.
-     */
+
     function transfer(address to, uint256 amount) public returns (bool) {
         // Enforce a condition that the sender has enough balance.
         require(_balances[msg.sender] >= amount, "ERC20: transfer amount exceeds balance");
